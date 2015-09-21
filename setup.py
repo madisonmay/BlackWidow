dc9012a808fe2bdb5080b4aac234899bb422586d
Madison May <madison@indico.io>
2015-09-21 00:31:00 -0400

    UPDATE: version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="blackwidow",
-    version="0.1.1",
+    version="0.1.2",
     author="Madison May",
     author_email="madison@indico.io",
     packages=find_packages(),
